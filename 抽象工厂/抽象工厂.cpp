0feead8970d663b42cc7b210cf8f97df71e657ef
zzdhhh <https://github.com/zhenda-hub/MyFirstGame.git>
2021-11-02 17:20:29 +0800

    添加项目文件。

---
+++ 抽象工厂/抽象工厂.cpp
@@ -0,0 +1,145 @@
+﻿// 抽象工厂.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+using namespace std;
+//产品球
+class AbstractBall
+{
+
+public:
+	AbstractBall() {};
+	void play() {};
+};
+
+class Basketball :public AbstractBall
+{
+public:
+	Basketball()
+	{
+		play();
+	}
+	void play()
+	{
+		printf("ZZD play basketball\n");
+	}
+};
+class Football :public AbstractBall
+{
+public:
+	Football()
+	{
+		play();
+	}
+	void play()
+	{
+		printf("ZZD play football\n");
+	}
+};
+
+//产品衬衫
+class ABstractShirt
+{
+public:
+	ABstractShirt() {};
+	void wearShirt() {};
+};
+
+class BasketballShirt : public ABstractShirt
+{
+public:
+	BasketballShirt()
+	{
+		wearShirt();
+	}
+	void wearShirt()
+	{
+		printf("ZZD wear Basketball shirt\n\n");
+	}
+};
+class FootballShirt : public ABstractShirt
+{
+public:
+	FootballShirt()
+	{
+		wearShirt();
+	}
+	void wearShirt()
+	{
+		printf("ZZD wear Football shirt\n\n");
+	}
+};
+
+//工厂
+class AbstractFactory
+{
+public:
+	virtual AbstractBall* getBall() = 0;
+	virtual ABstractShirt* getShirt() = 0;
+};
+
+class BasketballFactory :public AbstractFactory
+{
+public:
+	BasketballFactory()
+	{
+		printf("BasketballFactory\n");
+	}
+
+	AbstractBall* getBall()
+	{
+		printf("ZZD get basketball\n");
+		return new Basketball();
+	}
+
+	ABstractShirt* getShirt()
+	{
+		printf("ZZD get basketball shirt\n");
+		return new BasketballShirt();
+	}
+};
+class FootballFactory :public AbstractFactory
+{
+public:
+	FootballFactory()
+	{
+		printf("FootballFactory\n");
+	}
+
+	AbstractBall* getBall()
+	{
+		printf("ZZD get football\n");
+		return new Football();
+	}
+
+	ABstractShirt* getShirt()
+	{
+		printf("ZZD get football shirt\n");
+		return new FootballShirt();
+	}
+};
+
+int main()
+{
+	printf("抽象工厂开始\n");
+
+	AbstractFactory* fac = NULL;
+	AbstractBall* ball = NULL;
+	ABstractShirt* shirt = NULL;
+
+	fac = new BasketballFactory();
+	ball = fac->getBall();
+	shirt = fac->getShirt();
+
+	fac = new FootballFactory();
+	ball = fac->getBall();
+	shirt = fac->getShirt();
+
+
+
+
+	system("pause");
+	return 0;
+}
+
+
---
+++ 简单工厂/简单工厂.cpp
@@ -0,0 +1,184 @@
+﻿// 简单工厂.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+class AbstractProduct
+{
+public:
+	AbstractProduct() {}
+
+	void printName() {};//抽象方法
+
+	void play() {};
+};
+
+class Basketball :public AbstractProduct
+{
+public:
+	Basketball()
+	{
+		printName();
+		play();
+	}
+
+	void printName()
+	{
+		printf("ZZD get Basketball\n");
+	}
+
+	void play()
+	{
+		printf("ZZD play Basketball\n");
+	}
+
+};
+class Football :public AbstractProduct
+{
+public:
+	Football()
+	{
+		printName();
+		play();
+	}
+
+	void printName()
+	{
+		printf("ZZD get Football\n");
+	}
+
+	void play()
+	{
+		printf("ZZD play Football\n");
+	}
+
+};
+class Volleyball :public AbstractProduct
+{
+public:
+	Volleyball()
+	{
+		printName();
+		play();
+	}
+
+	void printName()
+	{
+		printf("ZZD get Volleyball\n");
+	}
+
+	void play()
+	{
+		printf("ZZD play Volleyball\n");
+	}
+
+};
+
+
+//factory
+class AbstractFactory
+{
+public:
+	virtual AbstractProduct* getSportProduct() = 0;//???
+
+	//AbstractProduct *getSportProduct(string productName)
+	//{
+	//	AbstractProduct* pro = NULL;
+	//	if (productName == "Basketball")
+	//	{
+	//		pro = new Basketball();//父类指针 = new 子类
+	//	}
+	//	else if (productName == "Football")
+	//	{
+	//		pro = new Football();
+	//	}
+	//	else if (productName == "Volleyball")
+	//	{
+	//		pro = new Volleyball();
+	//	}
+	//	return pro;
+	//}
+
+};
+
+class BasketballFactory :public AbstractFactory
+{
+public:
+	BasketballFactory()
+	{
+		cout << "BasketballFactory" << endl;
+	}
+
+	AbstractProduct* getSportProduct()
+	{
+
+		return new Basketball();
+	}
+
+};
+class FootballFactory :public AbstractFactory
+{
+public:
+	FootballFactory()
+	{
+		cout << "FootballFactory" << endl;
+	}
+
+	AbstractProduct* getSportProduct()
+	{
+
+		return new Football();
+	}
+
+};
+class VolleyballFactory :public AbstractFactory
+{
+public:
+	VolleyballFactory()
+	{
+		cout << "VolleyballFactory" << endl;
+	}
+
+	AbstractProduct* getSportProduct()
+	{
+
+		return new Volleyball();
+	}
+
+};
+
+
+int main()
+{
+	//简单
+	cout << "简单工厂模式" << endl;
+	/*Factory *fac = new Factory();
+	AbstractProduct *product = NULL;
+
+	product = fac->getSportProduct("Basketball");
+	product = fac->getSportProduct("Football");
+	product = fac->getSportProduct("Volleyball");*/
+
+	//方式
+	AbstractFactory* fac = NULL;
+	AbstractProduct* product = NULL;
+
+	fac = new BasketballFactory();
+	product = fac->getSportProduct();
+	fac = new FootballFactory();
+	product = fac->getSportProduct();
+	fac = new VolleyballFactory();
+	product = fac->getSportProduct();
+
+
+	system("pause");
+	return 0;
+}
+
+
+
+
